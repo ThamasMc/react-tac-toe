5a49a2bd1b3313ebec37bdf1bda47b28cf04c5db
Thamas McGowan <thamas.mcgowan@constantcontact.com>
2023-10-22 13:12:35 -0400

    Brings back the value prop
            - Moves state to the parent Board

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import { useState } from 'react';
 
-function Square() {
-  const [val, setVal] = useState(null);
+function Square({ val }) {
 
   function fooClick() {
-    alert('This is annoying');
+    console.log('We are going to change this in a sec');
   }
 
   return(
@@ -17,24 +16,33 @@ function Square() {
   );
 }
 
+/* React words of wisdom: 
+ * To collect data from multiple children, or to have two child components communicate with each other,
+ * declare the shared state in their parent component instead. The parent component can pass that state
+ * back down to the children via props. This keeps the child components in sync with each other and with their parent.
+ *
+ * Lifting state into a parent component is common when React components are refactored. */
+
 export default function Board() {
+  const [spaces, setSpaces] = useState(Array(9).fill(0));
   let count = 0;
+  
   return (
     <>
       <div class="board-row">
-        <Square  />
-        <Square  />
-        <Square  />
+        <Square val={spaces[count++]} />
+        <Square val={spaces[count++]} />
+        <Square val={spaces[count++]} />
       </div>
       <div class="board-row">
-        <Square  />
-        <Square  />
-        <Square  />
+        <Square val={spaces[count++]} />
+        <Square val={spaces[count++]} />
+        <Square val={spaces[count++]} />
       </div>
       <div class="board-row">
-        <Square  />
-        <Square  />
-        <Square  />
+        <Square val={spaces[count++]} />
+        <Square val={spaces[count++]} />
+        <Square val={spaces[count++]} />
       </div>
     </>
   );
